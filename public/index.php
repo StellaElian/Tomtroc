39cdf3d1189a5fe35773515289ed652c15b8f283
Stella Elian <elian_stella@yahoo.com>
2026-02-04 00:51:06 +0100

    modification de la variable controller pour être + précise

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@ require_once '../config/_config.php.php';
 require_once '../config/autoload.php';
 require_once '../src/managers/DBManager.php';
 
-$controller = new UserController();
-$controller->showFirstUser();
+$usercontroller = new UserController();
+$usercontroller->showFirstUser();
