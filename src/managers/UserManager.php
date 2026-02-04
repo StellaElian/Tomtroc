d8770867ef301b24d7068fc34f3d66392fd437f0
Stella Elian <elian_stella@yahoo.com>
2026-02-04 20:10:34 +0100

    functions réupération de l'id et de l'email

--- src/managers/UserManager.php
+++ src/managers/UserManager.php
@@ -1,9 +1,20 @@
 <?php
 class UserManager extends AbstractEntityManager
 {
-    public function getFirstUser(): ?User 
+    public function getUserById(int $id): ?User 
     {
-        $sql = "SELECT * FROM users LIMIT 1";
+        $sql = "SELECT * FROM users WHERE $id = :id";
+        $query = $this->db->query($sql);
+        $data = $query->fetch();
+        if ($data){
+            return new User($data);
+        }else{
+            return null;
+        }
+    }
+    public function getUserByEmail(string $email): ?User
+    {
+        $sql = "SELECT * FROM users WHERE $email = :email";
         $query = $this->db->query($sql);
         $data = $query->fetch();
         if ($data){
