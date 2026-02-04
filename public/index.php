3edc327a218912388667c2a2644f8b2757268bb2
Stella Elian <elian_stella@yahoo.com>
2026-02-04 01:14:20 +0100

    erreur d'écriture

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'config/_config.php';
-require_once 'config/autoload.php';
+require_once '../config/_config.php';
+require_once '../config/autoload.php';
 require_once '../src/managers/DBManager.php';
 
 $userController = new UserController();
-$useCcontroller->showFirstUser();
+$userCcontroller->showFirstUser();
