30cccdd511164ec16aa083b09e328afe0d4743a3
Stella Elian <elian_stella@yahoo.com>
2026-02-04 20:11:30 +0100

    fonction showProfile + test

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -1,9 +1,23 @@
 <?php
-class UserController {
-    public function showFirstUser(): void {
-        //creation du manager pour chercher l'utilisateur
-        $userManager = new UserManager();
-        $user = $userManager->getFirstUser();
-        require_once '../src/templates/home.php';
+class UserController
+{
+    public function showProfile(): void
+    {
+        if(isset($_SESSION['user_id'])){
+            $usermanager = new UserManager();
+            $user = $userManager->getUserById(['user_id']);
+            require_once '../src/templates/profile.php';
+        }else{
+            header(Location:'index.php?action=register');
+        }
     }
+
+    //test rapide 
+    public function testLogin(): void 
+    {
+        $_SESSION['user_ifd'] = 1;
+        echo "Tu es connecté user 1";
+        echo "<br><ahref='index.php?action=ptofile'>Voir mon profil</a>";
+    }
+
 }
