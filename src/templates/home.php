8e28028ef9c590ddd70f2a5646d9c83612cf70ce
Stella Elian <elian_stella@yahoo.com>
2026-02-06 04:23:04 +0100

    modification de _home.php

--- src/templates/home.php
+++ src/templates/home.php
@@ -5,9 +5,19 @@
         <title>TomTroc - Acceuil</title>
     </head>
     <body>
-        <h1>Bienvenue sur TomTroc !</h1>
-        <p>Le site d'échange de livres . </p>
-        <a href="index.php?action=register">Créer un compte</a>
-        <a href="index.php?action=login">Se connecter</a>
+        <?php require_once '../src/templates/_header.php'; ?>
+        <div class="home-container">
+            <section class="hero">
+                <h1>Bienvenue sur TomTroc !</h1>
+                <p>Le site d'échange de livres entre passionnés. </p>
+                <a href="index.php?action=register">Créer un compte</a>
+                <a href="index.php?action=login">Se connecter</a>
+                <?php if (!Utils::isUserConnected()): ?>
+                    <a href="index.php?action=register" class="btn-primary">Créer un compte</a>
+                <?php endif; ?>
+            </section>
+        </div>
+        <?php require_once '../src/templates/_footer.php'; ?>
+
     </body>
 </html>
