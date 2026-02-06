cedcff169906616f1b1e435f5e0c9c8aa595a6ad
Stella Elian <elian_stella@yahoo.com>
2026-02-06 11:27:26 +0100

    modification de login.php

--- src/templates/login.php
+++ src/templates/login.php
@@ -5,24 +5,23 @@
         <title>Connexion</title>
     </head>
     <body>
-        <nav>
-            <a href="index.php?action=home">Acceuil</a>
-        </nav>
+        <?php require_once '../src/templates/_header.php'; ?>
         <div class="login-container">
             <h1>Se connecter</h1>
 
             <form action="index.php?action=connectUser" method="post">
-                <label>Email</label><br>
-                <input type="email" name="email"><br><br>
-            
-                <label>Mot de passe</label><br>
-                <input type="password" name="password"><br><br>
-
+                <p>
+                    <label for="email">Email</label><br>
+                    <input type="email" name="email" id="email" required><br><br>
+                </p>
+                <p>
+                    <label for="password">Mot de passe</label><br>
+                    <input type="password" name="password" id="password" required><br><br>
+                </p>
                 <button type="submit">Se connecter</button>
-            
-                <p>Pas de compte ? <a href="index.php?action=register" style="test-decoration: underline; color: black;">Inscrivez-vous</a></p>
-                
             </form>
+            <p>Pas de compte ? <a href="index.php?action=register" style="test-decoration: underline; color: black;">Inscrivez-vous</a></p>
         </div>
+        <?php require_once '../src/templates/_footer.php'; ?>
     </body>
-</html>
+</html> 
