5f0932157f375cef356ad4c508cee2b3ff5ccb41
Stella Elian <elian_stella@yahoo.com>
2026-02-06 04:08:44 +0100

    creation de _footer

--- src/templates/_footer.php
+++ src/templates/_footer.php
@@ -1,3 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-    
+</main> <footer>
+    <div class="footer-content">
+        <p>&copy; 2024 TomTroc - Le site d'échange de livres</p>
+        <p>
+            <a href="#">Mentions légales</a>
+            <a href="#">Politique de confidentialité</a>
+        </p>
+    </div>
+</footer>
+</body> </html>
