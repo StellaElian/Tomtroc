1686006158208e0a1104c1add3dd5a12972b3074
Stella Elian <elian_stella@yahoo.com>
2026-02-06 12:00:53 +0100

    modification de register.php

--- src/templates/register.php
+++ src/templates/register.php
@@ -8,22 +8,27 @@
         <nav>
             <a href="index.php?action=home">Acceuil</a>
         </nav>
+        <?php require_once '../src/templates/_header.php'; ?>
         <div class="register-container">
             <h1>Créer un compte</h1>
-            <form action="index.php?action=registerUser" method="POST">
-                <label>Pseudo</label><br>
-                <input type="text" name="pseudo"><br><br>
-
-                <label>Adresse email</label><br>
-                <input type="email" name="email"><br><br>
-
-                <label>Mot de passe</label><br>
-                <input type="password" name="password"><br><br>
+            <form action="index.php?action=registerUser" method="post">
+                <p>
+                    <label for="pseudo">Pseudo</label><br>
+                    <input type="text" name="pseudo" id="pseudo" required><br><br>
+                </p>
+                <p>
+                    <label for="email">Adresse email</label><br>
+                    <input type="email" name="email" id="email" required><br><br>
+                </p>
+                <p>
+                    <label for="password">Mot de passe</label><br>
+                    <input type="password" name="password" id="password" required><br><br>
+                </p>
 
                 <button type="submit">S'inscrire</button>
-
-                <p>Déja inscrit ? <a href="index.php?action=login" style="test-decoration: underline; color: black;">Connectez-vous</a></p>
             </form>
+            <p>Déja inscrit ? <a href="index.php?action=login" style="test-decoration: underline; color: black;">Connectez-vous</a></p>
         </div>
+        <?php require_once '../src/templates/_footer.php'; ?>
     </body>
 </html>
