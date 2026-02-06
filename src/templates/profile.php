6b1b699213a9d07f2bafced623a669d1f0658f14
Stella Elian <elian_stella@yahoo.com>
2026-02-06 03:52:29 +0100

    modifications de la vue afin de correspondre à la maquette

--- src/templates/profile.php
+++ src/templates/profile.php
@@ -1,21 +1,66 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Mon compte</title>
-    </head>
-    <body>
-        <nav>
-            <a href="index.php?action=home">Acceuil</a>
-            <a href="index.php?action=login">Connexion</a>
-            <a href="index.php?action=logout">Se déconnecter</a>
-        </nav>
-        <h1>Mon compte</h1>
-        <div class="infos">
-            <img src="../public/img/avatars/<?= htmlspecialchars($user->getAvatar()) ?>" alt="Avatar" width="100"><br>
-            <p><a href="index.php?action=updateAvatar" style="test-decoration: underline; color: black;">modifier</a></p>
-            <p><strong><?= htmlspecialchars($user->getPseudo()) ?></p>
-            <p>Membre depuis<?= htmlspecialchars($user->getCreatAt()) ?></p>
+<?php require_once '../src/templates/_header.php'; ?>
+
+<div class="profile-page">
+    <h1>Mon compte</h1>
+
+    <div class="profile-content">
+        <div class="profile-card">
+            <div class="avatar-display">
+                <img src="../public/img/avatars/<?= htmlspecialchars($user->getAvatar()) ?>" alt="Avatar" width="150">
+                <br>
+                <a href="#">modifier</a>
+            </div>
+            <hr>
+            <h2><?= htmlspecialchars($user->getPseudo()) ?></h2>
+            <p>Membre depuis <?= Utils::format($user->getCreatedAt()) // Il faudra formater la date ?></p>
+            
+            <div class="library-count">
+                <strong>BIBLIOTHEQUE</strong><br>
+                0 livres
+            </div>
         </div>
-    </body>
-</html>
+
+        <div class="profile-form">
+            <h3>Vos informations personnelles</h3>
+            
+            <form action="index.php?action=updateProfile" method="post" enctype="multipart/form-data">
+                <label for="email">Adresse email</label><br>
+                <input type="email" name="email" value="<?= htmlspecialchars($user->getEmail()) ?>" required>
+                <br><br>
+
+                <label for="password">Mot de passe</label><br>
+                <input type="password" name="password" placeholder=".........">
+                <small>(Laissez vide si vous ne voulez pas le changer)</small>
+                <br><br>
+
+                <label for="pseudo">Pseudo</label><br>
+                <input type="text" name="pseudo" value="<?= htmlspecialchars($user->getPseudo()) ?>" required>
+                <br><br>
+
+                <button type="submit" class="btn-save">Enregistrer</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="user-books">
+        <table>
+            <thead>
+                <tr>
+                    <th>PHOTO</th>
+                    <th>TITRE</th>
+                    <th>AUTEUR</th>
+                    <th>DESCRIPTION</th>
+                    <th>DISPONIBILITÉ</th>
+                    <th>ACTION</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colspan="6">Vous n'avez pas encore de livres (Fonctionnalité à venir à l'étape 4)</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php require_once '../src/templates/_footer.php'; ?>
