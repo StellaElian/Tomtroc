92a7ec90f22ac62aa9cf0e1a5bb676a512db634b
Stella Elian <elian_stella@yahoo.com>
2026-02-15 18:14:47 +0100

    header

--- src/templates/_header.php
+++ src/templates/_header.php
@@ -1,26 +1,67 @@
+<?php
+require_once __DIR__ . '/../services/Utils.php';
+// On importe ta classe Utils pour pouvoir l'utiliser
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TomTroc</title>
-    <link rel="stylesheet" href="../public/css/style.css"> 
+    <title>Menu</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+
 </head>
+
 <body>
     <header>
-        <div class="logo">
-            <a href="index.php?action=home">TomTroc</a>
-        </div>
-        
         <nav>
-            <a href="index.php?action=home">Accueil</a>
-            <a href="index.php?action=exchange">Nos livres à l'échange</a>
-            <a href="index.php?action=profile">Mon compte</a>
+            <div class="spacer"></div>
+
+            <a href="index.php?action=home" class="logo">
+                <img src="img/min/logo.svg" alt="Logo TomTroc">
+            </a>
+
+            <a href="index.php?action=home" class="home <?= (isset($_GET['action']) && $_GET['action'] === 'home') ? 'active' : '' ?>">
+                Accueil
+            </a>
+
+            <div class="spacer"></div>
+
+            <a href="index.php?action=exchange" class="books link exchangeBooks <?= (isset($_GET['action']) && $_GET['action'] === 'exchange') ? 'active' : '' ?>">
+                Nos livres à l’échange
+            </a>
+
             <?php if (Utils::isUserConnected()): ?>
-                <li><a href="index.php?action=messagerie">Messagerie</a></li>
-                <li><a href="index.php?action=logout">Déconnexion</a></li>
+
+                <div class="spacer"></div>
+
+                <img src="img/min/line.svg" alt="séparateur" class="separator">
+
+                <a href="index.php?action=messagerie" class="messaging <?= (isset($_GET['action']) && $_GET['action'] === 'messagerie') ? 'active' : '' ?>">
+                    <img src="img/min/icon_messagerie.svg" alt="Messagerie" class="icon">
+                    <span class="messagingTxt">Messagerie</span>
+                    <img src="img/min/group_23.svg" alt="" class="group-icon">
+                </a>
+
+                <a href="index.php?action=profile" class="account <?= (isset($_GET['action']) && $_GET['action'] === 'profile') ? 'active' : '' ?>">
+                    <img src="img/min/icon_mon_compte.svg" alt="Compte">
+                    <span class="accountTxt">Mon compte</span>
+                </a>
+
+                <a href="index.php?action=logout" class="connexion">Déconnexion</a>
             <?php else: ?>
-                <a href="index.php?action=login">Connexion</a>
+            <div class="spacer"></div>
+            <div class="spacer"></div>
+            <div class="spacer"></div>
+            <div class="spacer"></div>
+            <a href="index.php?action=login" class="connexion <?= (isset($_GET['action']) && $_GET['action'] === 'login') ? 'active' : '' ?>">Connexion</a>
             <?php endif; ?>
+            <div class="spacer"></div>
         </nav>
     </header>
+
+    <main id="main"></main>
