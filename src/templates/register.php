7c2e5037fd73964f411bc1081ccc116c0c169b3b
Stella Elian <elian_stella@yahoo.com>
2026-02-20 14:30:27 +0100

    vue+css de register -validé

--- src/templates/register.php
+++ src/templates/register.php
@@ -1,34 +1,50 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Inscription - Tomtroc</title>
-    </head>
-    <body>
-        <nav>
-            <a href="index.php?action=home">Acceuil</a>
-        </nav>
-        <?php require_once '../src/templates/_header.php'; ?>
-        <div class="register-container">
-            <h1>Créer un compte</h1>
-            <form action="index.php?action=registerUser" method="post">
-                <p>
-                    <label for="pseudo">Pseudo</label><br>
-                    <input type="text" name="pseudo" id="pseudo" required><br><br>
-                </p>
-                <p>
-                    <label for="email">Adresse email</label><br>
-                    <input type="email" name="email" id="email" required><br><br>
-                </p>
-                <p>
-                    <label for="password">Mot de passe</label><br>
-                    <input type="password" name="password" id="password" required><br><br>
-                </p>
 
-                <button type="submit">S'inscrire</button>
-            </form>
-            <p>Déja inscrit ? <a href="index.php?action=login" style="text-decoration: underline; color: black;">Connectez-vous</a></p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<body>
+    <?php require_once '../src/templates/_header.php'; ?>
+    <main class="main-auth">
+        <div class="auth">
+            <div class="auth-left">
+                <h1 class="sign-title">Inscription</h1>
+
+                <form action="index.php?action=registerUser" method="post" class="sign-form">
+                    <div class="field">
+                        <label for="pseudo">Pseudo</label>
+                        <input type="text" name="pseudo" id="pseudo" required>
+                    </div>
+
+                    <div class="field">
+                        <label for="email">Adresse email</label>
+                        <input type="email" name="email" id="email" required>
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+
+                    <button type="submit" class="sign-submit-btn">S'inscrire</button>
+                </form>
+
+                <div class="sign-ask">
+                    Déjà inscrit ? <a href="index.php?action=login">Connectez-vous</a>
+                </div>
+            </div>
+
+            <img src="img/books/image_inscription.png" class="auth-img" alt="Bibliothèque">
         </div>
-        <?php require_once '../src/templates/_footer.php'; ?>
-    </body>
+    </main>
+
+    <?php require_once '../src/templates/_footer.php'; ?>
+</body>
+
 </html>
