3082e5b2a8464863d0c2431dbbb319bd25f72ea4
Stella Elian <elian_stella@yahoo.com>
2026-03-08 11:14:19 +0100

    visibilité de _config.php  pour examinateur

---
+++ config/_config.php
@@ -0,0 +1,9 @@
+<?php 
+//sans ça, le site oublie tout à chaque fois 
+session_start();
+
+// données de connexion à la bdd
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tomtroc');
+define('DB_USER', 'root');
+define('DB_PASS', '');
